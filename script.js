a6b74605f72654501ad1d826067a185aebe6da87
talaksew misganaw <talaksewmisganaw1@gmail.com>
2025-05-30 02:48:07 +0300

    displaying project detail dynamically in JS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+const projectsTab = document.getElementById("projects-tab");
 const projectsContainer = document.querySelector(".projects");
 const menusContainer = document.querySelector(".menus");
 const menus = document.querySelectorAll(".menu");
@@ -22,7 +23,7 @@ const themeBtn = document.querySelector(".theme-changer");
 
 
 // ***************************
-const projectDetail = document.querySelector(".project-detail");
+
 
 
 // ***************************
@@ -40,6 +41,7 @@ window.addEventListener("load", () => {
     // body.style.overflow="hidden";
 })
 
+//theme changer
 themeBtn.addEventListener("click", () => {
     body.classList.toggle("dark-theme");
 })
@@ -48,7 +50,7 @@ themeBtn.addEventListener("click", () => {
 //     tab.classList.contains("section-tab")?tab.classList.remove("section-tab"):tab.classList.add("section-tab");
 // })
 
-const projectsList = [{
+const projectsList = {"lalibela-hotel": {
     imageUrl: `images/lalibela-hotel.png`,
     title: "Lalibela Hotel",
     subtitle1: "10<sup>th</sup> semester project",
@@ -60,9 +62,13 @@ const projectsList = [{
     wisdom. The concept of this project is adapting 
     lalibela architecture to contemporary architecture 
     in hotel design according to primary elements of 
-    design.`
+    design.`,
+    formDevelopementImage: "./images/HRH-form-dev.png",
+    detailParagraph1: "This project is integrated design project by integrating system with buildings. The concept of this project is intervention - intervention between nature and man made thing(building)",
+    detailParagraph2: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis iste deserunt voluptate, consequuntur id ut, nemo veritatis ad odio iusto illum libero ducimus commodi sequi maxime cum repudiandae, saepe aspernatur eum autem error dolorum. Amet consectetur doloremque praesentium, eos tempore odio suscipit quaerat repellat dolor iste, dolores nihil? Porro dolorem magni, rerum eaque in excepturi error amet a minus voluptatum consequuntur consectetur aperiam consequatur dolorum dolores non, dolor ullam. Dolorem beatae vero similique facere amet ea neque totam aperiam officia rerum non doloribus dolorum adipisci ipsam sint eveniet blanditiis cupiditate, in delectus aliquam perspiciatis corrupti? Culpa amet voluptatem deserunt odit.</p>",
+    detailPhoto: ["images/HRH-photo-1.png", "images/HRH-photo-2.png"]
 },
-{
+"high-rise-hotel": {
     imageUrl: `images/lalibela-hotel.png`,
     title: "High Rise Hotel",
     subtitle1: "9<sup>th</sup> semester project",
@@ -71,9 +77,10 @@ const projectsList = [{
     description: `This project is integrated design project by
     integrating system with buildings. The concept of this
     project is intervention - intervention between nature 
-    ana man made thing(building)`
-}, 
-{
+    ana man made thing(building)`,
+    formDevelopementImage: "./images/HRH-form-dev.png"
+},
+"rehabilitation-center-design":{
     imageUrl: `images/lalibela-hotel.png`,
     title: "Rehabilitation Center Design",
     subtitle1: "Freelance Project",
@@ -84,9 +91,10 @@ const projectsList = [{
     by design. In this project my work was the landscape 
     part. I worked this project in collaboration with my 
     friend. Although we worked most part of the project 
-    toghether, the role of working the landscape was mine`
+    toghether, the role of working the landscape was mine`,
+    formDevelopementImage: "./images/HRH-form-dev.png"
 },
-{
+"residence-design":{
     imageUrl: `images/lalibela-hotel.png`,
     title: "Residence Design",
     subtitle1: "Internship Project",
@@ -95,11 +103,12 @@ const projectsList = [{
     description: `Due to the order of the client, this 
     project has ground plus two floors. As the client's 
     wish, the ground floor is complete family residential 
-    floor for the client in order to reside.`
+    floor for the client in order to reside.`,
+    formDevelopementImage: "./images/HRH-form-dev.png"
 },
-{
+"residence-design-ii":{
     imageUrl: `images/lalibela-hotel.png`,
-    title: "Residence Design",
+    title: "Residence Design II",
     subtitle1: "Internship Project",
     year: "2015 E.C.",
     location: "Woldiya",
@@ -108,9 +117,10 @@ const projectsList = [{
     programs. This project was the one developed my skills 
     on personal residential projects. It helped me know 
     dominant people's need of how the form of their 
-    residential building to be.`
+    residential building to be.`,
+    formDevelopementImage: "./images/HRH-form-dev.png"
 },
-{
+"landscape-design":{
     imageUrl: `images/lalibela-hotel.png`,
     title: "Landscape Design",
     subtitle1: "Freelance Project",
@@ -122,12 +132,13 @@ const projectsList = [{
     landscape comfortable for the owners regarding the 
     dignity and respect for the pops, and also making it 
     suitable for the events and any situatios which could 
-    be organized there.`
+    be organized there.`,
+    formDevelopementImage: "./images/HRH-form-dev.png"
 }
-];
+};
 
-
-projectsList.forEach(item => {
+for (let key in projectsList) {
+    let item = projectsList[key];
     const project = document.createElement("div");
     project.id = item.title.toLowerCase().replace(/ /g, "-");
     project.classList.add("project");
@@ -146,20 +157,207 @@ projectsList.forEach(item => {
     </p>
     </div>
     <div class="hidden-layer">
-    <button class="view-btn">View Full Project<span class="view-icon"></span><i class="fa-solid fa-chevron-right"></i><i class="fa-solid fa-chevron-right"></i></button>
+    <button class="view-btn">View Full Project<div class="view-icons"><i class="fa-solid fa-chevron-right"></i><i class="fa-solid fa-chevron-right"></i></div></button>
     </div>
     `
     
     projectsContainer.appendChild(project)
-})
+}
+// console.log(projectsList[0])
+
+// projectsList.forEach(item => {
+//     const project = document.createElement("div");
+//     project.id = item.title.toLowerCase().replace(/ /g, "-");
+//     project.classList.add("project");
+//     project.innerHTML = `
+//     <div class="image-container">
+//     <img src="${item.imageUrl}" alt="${project.id}">
+//     </div>
+//     <div class="text-container">
+//     <h1 class="title">${item.title}</h1>
+//     <div class="sub-titles">
+//     <h2 class="sb-title sb-title-1">${item.subtitle1}</h2>
+//     <h2 class="sb-title year">Year: ${item.year}</h2>
+//     </div>
+//     <p class="description">
+//     ${item.description}
+//     </p>
+//     </div>
+//     <div class="hidden-layer">
+//     <button class="view-btn">View Full Project<div class="view-icons"><i class="fa-solid fa-chevron-right"></i><i class="fa-solid fa-chevron-right"></i></div></button>
+//     </div>
+//     `
+    
+//     projectsContainer.appendChild(project)
+// })
 
 
 //opening full view
 const viewBtn = document.querySelectorAll(".view-btn");
+let projectDetail;
 viewBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        // console.log(body.style.height)
+    btn.addEventListener("click", (e) => {
+        const detailElementId = e.currentTarget.parentElement.parentElement.id;
+        let detailItem = projectsList[detailElementId];
+        console.log(detailElementId);
+        console.log(detailItem)
+
         fanayeText.style.display="none";
+        const detailElement = `
+        <div class="project-detail">
+            <div class="detail-description detail">
+                <h1 class="detail-title">
+                    ${detailItem.title}
+                </h1>
+                <div class="caption-and-form">
+                    <ul class="detail-caption">
+                        <li class="caption-1">${detailItem.subtitle1}</li>
+                        <li class="year">Year: ${detailItem.year}</li>    
+                        <li class="location">Location: ${detailItem.location}</li>
+                    </ul>
+                    <div class="form-developement">
+                        <img src=${detailItem.formDevelopementImage} alt="form-developement">
+                    </div>
+                </div>
+                <p class="detail-text"> ${detailItem.detailParagraph1}
+                    <br/>
+                    <br/>
+                    ${detailItem.detailParagraph2}
+                    </div>
+            <div class="detail-photo detail">
+                <div class="images-container">
+                    <div class="prev prev-next-btn"><i class="fa-solid fa-chevron-left"></i></div>
+                    <div class="next prev-next-btn"><i class="fa-solid fa-chevron-right"></i></div>
+                    ${detailItem.detailPhoto.forEach(item => {
+                        const detailPhotoDiv = document.createElement("div");
+                        detailPhotoDiv.innerHTML=`<img src="${item}" alt="photo${item.index}">`;
+                        detailPhotoDiv.classList.add("image");
+                        if (item === detailItem.detailPhoto[0]) {
+                            detailPhotoDiv.classList.add("visible-photo");
+                        }
+                        console.log(detailPhotoDiv, item === detailItem.detailPhoto[0], item.index)
+                        return `${detailPhotoDiv}`;
+                        // console.log(detailPhotoDiv)
+                    })}
+                    // console.log(item === detailItem.detailPhoto[0]))}
+                    // <div class="image visible-photo">
+                    //     <img src="images/HRH-photo-1.png" alt="photo 1">
+                    // </div>
+                    // <div class="image">
+                    //     <img src="images/HRH-photo-2.png" alt="photo-2">
+                    // </div>
+                    <div class="dots">
+                        <div class="dot active-dot"></div>
+                        <div class="dot"></div>
+                    </div>
+                    <!-- <div class="divider"></div> -->
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="divider"></div>
+            <div class="detail-siteplan detail">
+                <h2>Site Plan</h2>
+                <div class="siteplan-image">
+                    <img src="images/HRH-siteplan.png" alt="siteplan">
+                </div>
+            </div>
+            <div class="detail-floorplan detail">
+                <h2>Floor Plans</h2>
+                <div class="floorplan">
+                    <div class="prev-floor prev-next-floor"><i class="fa-solid fa-chevron-left"></i></div>
+                    <div class="next-floor prev-next-floor"><i class="fa-solid fa-chevron-right"></i></div>
+                    <div class="floor-plan-img visible-floor">
+                        <img src="images/HRH-ground-floor.png" alt="ground floor" />
+                        <h3>Ground Floor</h3>
+                    </div>
+                    <div class="floor-plan-img">
+                        <img src="images/HRH-basement-floor.png" alt="basement floor" />
+                        <h3>Basement Floor</h3>
+                    </div>
+                    <div class="floor-plan-img">
+                        <img src="images/HRH-first-floor.png" alt="first floor" />
+                        <h3>First Floor</h3>
+                    </div>
+                    <div class="floor-plan-img">
+                        <img src="images/HRH-4th-20th-floor.png" alt="4th-20th floor" />
+                        <h3>4<sup>th</sup> to 20<sup>th</sup> Floor</h3>
+                    </div>
+                    <div class="floor-dots">
+                        <div class="floor-dot active-floor-dot"></div>
+                        <div class="floor-dot"></div>
+                        <div class="floor-dot"></div>
+                        <div class="floor-dot"></div>
+                    </div>
+                </div>
+                <button class="download-btn">Download CAD file <i class="fa-solid fa-download"></i></button>
+                <!-- <div class="floorplan">
+                    <img src="images/HRH-basement-floor.png" alt="ground floor" />
+                </div>
+                <div class="floorplan">
+                    <img src="images/HRH-first-floor.png" alt="first floor" />
+                </div>
+                <div class="floorplan">
+                    <img src="images/HRH-4th-20th-floor.png" alt="4th-20th floor" />
+                </div> -->
+            </div>
+            <div class="divider"></div>
+            <div class="divider"></div>
+            <div class="detail-elevation detail">
+                <h2>Elevations</h2>
+                <div class="elevation">
+                    <div class="prev-elev prev-next-elev"><i class="fa-solid fa-chevron-left"></i></div>
+                    <div class="next-elev prev-next-elev"><i class="fa-solid fa-chevron-right"></i></div>
+                    <div class="elevation-img visible-elev">
+                        <img src="images/HRH-front.png" alt="Front side Elevation" />
+                        <h3>Front View</h3>
+                    </div>
+                    <div class="elevation-img">
+                        <img src="images/HRH-rare.png" alt="Rare Elevation" />
+                        <h3>Rare View</h3>
+                    </div>
+                    <div class="elevation-img">
+                        <img src="images/HRH-left.png" alt="Left Elevation" />
+                        <h3>Left View</h3>
+                    </div>
+                    <div class="elevation-img">
+                        <img src="images/HRH-right.png" alt="Right Elevation" />
+                        <h3>Right View</h3>
+                    </div>
+                    <div class="elev-dots">
+                        <div class="elev-dot active-elev-dot"></div>
+                        <div class="elev-dot"></div>
+                        <div class="elev-dot"></div>
+                        <div class="elev-dot"></div>
+                    </div>
+                </div>
+                <button class="download-btn">Download CAD file <i class="fa-solid fa-download"></i></button>
+            </div>
+            <div class="detail-section detail">
+                <h2>Sections</h2>
+                <div class="section">
+                    <div class="prev-section prev-next-section"><i class="fa-solid fa-chevron-left"></i></div>
+                    <div class="next-section prev-next-section"><i class="fa-solid fa-chevron-right"></i></div>
+                    <div class="section-img visible-section">
+                        <img src="images/HRH-section-A-A.png" alt="section A-A" />
+                        <h3>section A-A</h3>
+                    </div>
+                    <div class="section-img">
+                        <img src="images/HRH-section-B-B.png" alt="section B-B" />
+                        <h3>section B-B</h3>
+                    </div>
+                    <div class="section-dots">
+                        <div class="section-dot active-section-dot"></div>
+                        <div class="section-dot"></div>
+                    </div>
+                </div>
+                <button class="download-btn">Download CAD file <i class="fa-solid fa-download"></i></button>
+            </div>
+        </div>
+        `
+        // projectsTab.appendChild(detailElement);
+
+        projectDetail = document.querySelector(".project-detail");
+
         projectDetail.classList.add("visible-detail");
         body.style.height="90vh";
         body.style.overflow="hidden";
